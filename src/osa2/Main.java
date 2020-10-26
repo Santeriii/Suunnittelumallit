65050070039bd83e90f8be5d9ff274ea2e823092
admin <admin@DESKTOP-PK7Q5UA>
2020-10-26 19:09:41 +0200

    2. osan "main"-luokkkaa korjattu

--- src/osa2/Main.java
+++ src/osa2/Main.java
@@ -6,21 +6,8 @@ import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args) {
-		Class c = null;
-		AdidasVaateFactoryIF adidasTehdas = null;
-		
-		Properties properties = new Properties();
-		try {
-			properties.load(
-			new FileInputStream("adidasTehdas.properties"));
-		} catch (IOException e) {e.printStackTrace();}
-			try{
-			//luetaan toteuttava tehdas properties-tiedostosta
-			c = Class.forName(properties.getProperty("adidasTehdas"));
-			adidasTehdas = (AdidasVaateFactoryIF)c.newInstance();
-		} catch (Exception e){e.printStackTrace();}
-		
-		Farkut adidasFarkut = adidasTehdas.makeFarkut();
+
+		Farkut adidasFarkut = new AdidasVaateFactory().makeFarkut();
 		Kengat adidasKengat = new AdidasVaateFactory().makeKengat();
 		Lippis adidasLippis = new AdidasVaateFactory().makeLippis();
 		TPaita adidasTPaita = new AdidasVaateFactory().makeTPaita();
@@ -30,14 +17,14 @@ public class Main {
 		System.out.println(adidasLippis.toString());
 		System.out.println(adidasTPaita.toString());
 		
-		Farkut bossFarkut = new BossVaateFactory().makeFarkut();
-		Kengat bossKengat = new BossVaateFactory().makeKengat();
-		Lippis bossLippis = new BossVaateFactory().makeLippis();
-		TPaita bossTPaita = new BossVaateFactory().makeTPaita();
+		adidasFarkut = new BossVaateFactory().makeFarkut();
+		adidasKengat = new BossVaateFactory().makeKengat();
+		adidasLippis = new BossVaateFactory().makeLippis();
+		adidasTPaita = new BossVaateFactory().makeTPaita();
 		
-		System.out.println(bossFarkut.toString());
-		System.out.println(bossKengat.toString());
-		System.out.println(bossLippis.toString());
-		System.out.println(bossTPaita.toString());
+		System.out.println(adidasFarkut.toString());
+		System.out.println(adidasKengat.toString());
+		System.out.println(adidasLippis.toString());
+		System.out.println(adidasTPaita.toString());
 	}
 }
