5e72ad5bd325d72c7f3b93b4e94e2810c1c69ad4
Santeri Einola <einola.santeri@gmail.com>
2020-12-06 18:04:15 +0200

    Facade Started

--- src/facade/CPU.java
+++ src/facade/CPU.java
@@ -7,7 +7,11 @@ public class CPU {
 	public void Freeze(Memory memory, int size, HDD hdd) {
 		memory.Load(hdd, size);
 	}
-	  /*void Jump(long position);
+	
+	
+	/*void Jump(int position) {
+		memory.Load(hdd, position)
+	}
 	  void Execute();*/
 	
 }
