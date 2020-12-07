f04bf296fca70c6585d13f42e6ddd5a4495d292a
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-12-07 16:57:00 +0200

    iterator-exercise done

--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -2,7 +2,6 @@ package iterator;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,10 +15,22 @@ public class Main {
     Iterator<String> it = cars.iterator();
     Iterator<String> it2 = cars.iterator();
 
-    MyThread threadOne = new MyThread(it, 6);
-    MyThread threadTwo = new MyThread(it2, 7);
+    MyThread threadOne = new MyThread(it);
+    MyThread threadTwo = new MyThread(it2);
     
     threadOne.start();
     threadTwo.start();
+    
+    /*MyThread threadOne = new MyThread(it);
+    MyThread threadTwo = new MyThread(it);
+    
+    threadOne.start();
+    threadTwo.start();*/
+    
+    /*MyThread threadOne = new MyThread(it);
+    
+    threadOne.start();*/
+    
+    
   }
 }
--- src/iterator/MyThread.java
+++ src/iterator/MyThread.java
@@ -1,26 +1,32 @@
 package iterator;
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class MyThread extends Thread {
 	
 	private Iterator<String> it;
-	int value;
+	private Scanner lukija = new Scanner(System.in);
 	
-	public MyThread (Iterator<String> it, int value) {
+	public MyThread (Iterator<String> it) {
 		this.it = it;
-		this.value = value;
 	}
 
 	@Override
 	public void run() {
-		while (it.hasNext()) printNumber(this.value);
+		int execute;
+		while (it.hasNext()) {
+			System.out.println("Jatka : syötä '1', odota : syötä '0', poista nykyinen iteroitava yksilö: 2");
+			execute = Integer.parseInt(lukija.nextLine());
+			if (execute == 1) {
+				printIteratorContents();
+			} else if (execute == 2) {
+				it.remove();
+			}
+		}
 	}
 	
-	public void printNumber(int number) {
-        if (value % 2 == 0) {
-            System.out.println(it.next());
-            value++;
-        }
-    }
+	public void printIteratorContents() {
+		System.out.println(it.next());
+	}
 }
